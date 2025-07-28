e3f3b14532c61e3a6c1ccf82a78c4a5f07e886e5
Gary Riches <gary@bouncingball.mobi>
2025-07-28 10:36:39 +0100

    Initial Commit

---
+++ oxiproimport/oxiproimportApp.swift
@@ -0,0 +1,17 @@
+//
+//  oxiproimportApp.swift
+//  oxiproimport
+//
+//  Created by Gary Riches on 28/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct oxiproimportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
